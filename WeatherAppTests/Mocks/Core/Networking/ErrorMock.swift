947c85175795c7ef42d8925adf9de29b75db83f0
haffez23 <haffez23@gmail.com>
2024-02-04 16:48:40 +0400

    Add NetworkingMock

---
+++ WeatherAppTests/Mocks/Core/Networking/ErrorMock.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMock.swift
+//  WeatherAppTests
+//
+//  Created by Mohamed Haffez on 04/02/2024.
+//
+
+import Foundation
+
+class ErrorMock:Error{
+    
+}
---
+++ WeatherAppTests/Mocks/Core/Networking/NetworkManagerMock.swift
@@ -0,0 +1,35 @@
+    //
+    //  NetworkManagerTests.swift
+    //  WeatherAppTests
+    //
+    //  Created by Mohamed Haffez on 04/02/2024.
+    //
+
+import Foundation
+@testable import WeatherApp
+
+class NetworkManagerMock: NetworkManagerExectable{
+    
+    
+    var invokedExecute = false
+    var invokedExecuteCount = 0
+    var invokedExecuteDataResult: Data?
+    
+    var errorData: Error?
+    var invokedExecuteWithErrorData:Data?
+    
+    func execute<T>(
+        _ request: NetworkRequest,
+        deserializer: @escaping (Data, [AnyHashable : Any]) throws -> T,
+        success: @escaping (T) -> Void,
+        failure: @escaping (Error) -> Void,
+        errorDeserializer: ((Data, Error) throws -> Error)?) where T : Decodable {
+            if let errorData{
+                failure(errorData)
+            }else if let invokedExecuteWithErrorData, let errorDeserializer{
+                failure(try! errorDeserializer(invokedExecuteWithErrorData, ErrorMock()))
+            }else {
+                success(invokedExecuteDataResult as! T)
+            }
+        }
+}
